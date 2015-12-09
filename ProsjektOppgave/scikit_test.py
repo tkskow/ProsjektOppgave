372b6d69c886dbcc6e000b8ed008c28b521338b7
Terje Kristoffer Skow <tkskow@gmail.com>
2015-12-09 19:44:38 +0100

    sync skole

--- ProsjektOppgave/scikit_test.py
+++ ProsjektOppgave/scikit_test.py
@@ -2,7 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.font_manager
 import csv
+from sklearn import metrics
 from scipy import stats
+from sklearn import preprocessing
 
 from sklearn import svm
 from sklearn.covariance import EllipticEnvelope
@@ -11,40 +13,59 @@ with open('first15000DnsCalls.csv','rb') as csvfile:
 	spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
 	temp = []
 	for row in spamreader:
-		one = int(row[1])
-		two = int(row[2])
-		three = int(row[3])
-		temp.append([one,two,three])
+		for i,x in enumerate(row):
+			if i > 0:
+				row[i] = float(row[i])
+		one = float(row[4])
+		two = float(row[5])
+		#three = int(row[3])
+		#temp.append([one,two])
+		temp.append(row)
 
+	data = np.array(temp)
 
-
+	#data = data.
 	#elliptic = EllipticEnvelope(contamination=.1)
 	#elliptic.fit(data)
 	#oneSVM = svm.LinearSVC()
-	oneSVM = svm.OneClassSVM(nu=0.16)
-	oneSVM.fit(temp)
+	oneSVM = svm.OneClassSVM(nu=0.005)
+	oneSVM.fit(data[:,1:])
 
 
 with open('restOfDnsCalls.csv','rb') as csvfile2:
 	spamreader = csv.reader(csvfile2, delimiter=',', quotechar='|')
 	temp = []
 	for row in spamreader:
-		one = int(row[1])
-		two = int(row[2])
-		three = int(row[3])
-		temp.append([one,two,three])
+		row[4] = float(row[4])
+		row[5] = float(row[5])
+		#print type(one)
+		#three = int(row[3])
+		#temp.append([one,two])
+		temp.append(row)
+		#data = np.array(temp)
+
+
+
 	data = np.array(temp)
-	print len(data)
+	#print len(data)
 	#for row in data:
 	#elliptic.predict(data)
-	prediction = oneSVM.predict(data)
+	#print data[:,4:]
+	prediction = oneSVM.predict(data[:,1:])
+
+	print oneSVM.get_params()
+	#print prediction, type(prediction)
 	negative = 0
 	positive = 0
-	for i in prediction:
+	for x,i in enumerate(prediction):
 		if i < 0:
 			negative += 1
+			print data[x,:]
 		else:
 			positive += 1
 	print positive, negative
 
-	print oneSVM.decision_function(data).ravel()
+	# for line in data:
+	# 	print (oneSVM.decision_function(line[1:]))
+
+	# print oneSVM.decision_function(data[:,1:]).ravel()
